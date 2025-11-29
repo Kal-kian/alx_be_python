5ce7c77a4efa15a3b4e823d49ee33ba6c865b903
kal-kian <kalkidanmisgadu@gmail.com>
2025-11-29 18:11:06 -0300

    check all requirments

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -2,7 +2,6 @@ import sys
 from bank_account import BankAccount
 
 def main():
-   
     account = BankAccount(100)
 
     if len(sys.argv) < 2:
@@ -10,17 +9,16 @@ def main():
         print("Commands: deposit, withdraw, display")
         sys.exit(1)
 
-   
     command, *params = sys.argv[1].split(':')
     amount = float(params[0]) if params else None
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
-        print(f"Deposited: ${amount}")
+        print(f"Deposited: ${amount:.2f}")
 
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew: ${amount}")
+            print(f"Withdrew: ${amount:.2f}")
         else:
             print("Insufficient funds.")
 
